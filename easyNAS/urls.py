59ee2fd30e7ed1f56dd00eee26e9efeaa0a82865
rjhowar2 <rjhowar2@gmail.com>
2016-01-13 02:12:30 -0600

    Static url fix
    adding correct url pattern for static files

--- easyNAS/urls.py
+++ easyNAS/urls.py
@@ -17,12 +17,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
-from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home', TemplateView.as_view(template_name='easyNAS/index.html'), name="nas_home"),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root', settings.STATIC_ROOT}
-    ),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
