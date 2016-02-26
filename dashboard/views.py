74f66fc8f9eef6406bbf1387ba5c2521980cc7fb
Ryan Howard <rhoward@Guaranteeds-MacBook-Air.local>
2016-02-25 21:59:35 -0600

    chaging api calls

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,28 +4,32 @@ from django.conf import settings
 
 import requests
 import urllib
+import json
 
 class DashboardView(TemplateView):
 
-    template_name = "dashboard/dashboard.html"
+    #template_name = "dashboard/dashboard.html"
+
+    def get_template_names(self):
+    	if self.request.is_ajax():
+        	return ["dashboard/folder_contents.html"]
+        else:
+        	return ["dashboard/dashboard.html"]
 
     def get_context_data(self, **kwargs):
+    	folder = self.request.GET.get('path','')
+
         context = super(TemplateView, self).get_context_data(**kwargs)
-        context['folder_contents'] = get_files_from_server()
+        context['folder_contents'] = get_files_from_server(folder)
+        context['api_urls'] = settings.FILE_SERVER_URLS
+        
         return context
 
-def get_files_from_server(path=""):
-	base_dir_url = settings.FILE_SERVER_URLS['BASE_DIR']
-
-	try:
-		base_dir = requests.get(base_dir_url).json()['base_directory']
-	except Exception, e:
-		base_dir = ''
-
-	args = {"path": base_dir}
+def get_files_from_server(folder):
+	args = {"path": folder}
 	url = "%s?%s" % (settings.FILE_SERVER_URLS['CONTENTS'], urllib.urlencode(args))
 
+	print "url - %s" % url
 	response = requests.get(url)
-
 	return response.json()
 
