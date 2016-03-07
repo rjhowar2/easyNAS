66085478b4f5949495e83b5755ece72c0b2cd102
Ryan Howard <rhoward@Guaranteeds-MacBook-Air.local>
2016-03-06 22:30:18 -0600

    fixing subnav and cleaning up some html

--- dashboard/static/dashboard/js/dashboard.js
+++ dashboard/static/dashboard/js/dashboard.js
@@ -1,6 +1,10 @@
 
 $(document).ready(function(){
 	$('[data-toggle="tooltip"]').tooltip(); 
+
+	$('#dashboard_menu').affix({
+  		offset: {top: 50}
+	});
 });
 
 var api_urls = JSON.parse(api_urls);
