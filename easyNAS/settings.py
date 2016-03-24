6c4945a6cbfb8701994a7252ca0a5cd43d0727f9
rjhowar2 <rjhowar2@gmail.com>
2016-03-23 20:51:47 -0500

    adding production ip

--- easyNAS/settings.py
+++ easyNAS/settings.py
@@ -133,9 +133,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-FILE_SERVER_BASE_URL = "http://127.0.0.1:5000/nas_server/api/v1.0"
-#FILE_SERVER_BASE_URL = "http://10.0.0.23:5000/nas_server/api/v1.0"
-#FILE_SERVER_BASE_URL = "http://71.57.23.72:5000/nas_server/api/v1.0"
+#FILE_SERVER_BASE_URL = "http://127.0.0.1:5000/nas_server/api/v1.0"
+FILE_SERVER_BASE_URL = "http://71.57.23.72:5000/nas_server/api/v1.0"
 
 FILE_SERVER_URLS = {
     'BASE_DIR': "%s/directory/root" % FILE_SERVER_BASE_URL,
