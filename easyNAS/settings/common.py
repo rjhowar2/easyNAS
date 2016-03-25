6418ce9625e8e428800ef99e2d9fdc0c8ebcec56
Ryan Howard <rhoward@Guaranteeds-MacBook-Air.local>
2016-03-25 15:22:49 -0500

    fixing settings base dir

--- easyNAS/settings/common.py
+++ easyNAS/settings/common.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(os.path.relpath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.relpath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
