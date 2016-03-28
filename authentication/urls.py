abce2124313ff6ec3f8631ed4b73a5ea088b7d4d
Ryan Howard <rhoward@Guaranteeds-MacBook-Air.local>
2016-03-27 20:21:58 -0500

    fixing login errors

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,7 +5,7 @@ from authentication.views import AuthenticationView, LoginView, logout_view
 
 urlpatterns = [
 	url(r'^$', LoginView.as_view(), name="login_screen"),
-    url(r'/(?P<error_code>[0-9]+)/$', LoginView.as_view(), name="login_error"),
+    url(r'(?P<error_code>[0-9]+)/$', LoginView.as_view(), name="login_error"),
     url(r'^authenticate/', AuthenticationView.as_view(), name="authenticate"),
     url(r'^logout', logout_view, name="logout_user"),
 ]
